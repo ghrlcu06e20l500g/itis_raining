6ae24187fbe1fcbaa4864a8d12655b22843b4d3a
ghrlcu06e20l500g <148538702+ghrlcu06e20l500g@users.noreply.github.com>
2023-11-09 22:15:22 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,67 +1,127 @@
-function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-}
-class Day {
-    constructor(date, weather, temperature) {
-        this.date = date;
-        this.weather = weather;
-        this.temperature = temperature;
-    }
-    card() {
-        return /* html */ `
-            <div>${this.date.getDate()}/${this.date.getMonth() + 1}/${this.date.getFullYear()}</div>
-            <div>${this.date.toLocaleDateString(undefined, {weekday: "long"})}</div>
-            <img src="images/${this.weather.toLowerCase()}.png">
-            <div>${capitalize(this.weather).replace(/_/g, ' ')} (${this.temperature}°C)</div>
-        `;
-    }
-}
+document.addEventListener("DOMContentLoaded", function() {
+    // .menu
+        $(".menu").each(function() {
+            $(this).data("active", $(this).find("button.active").text());
+        });
+        $(".menu button").click(function() {
+            $(this).closest(".menu").find("button").removeClass("active");
+            $(this).addClass("active");
 
-class Daytest {
-    constructor(ticks) {
-        this.ticks = ticks;
-    }
-    temperature() {
-        let sum = 0;
-        for(let i = 0; i < this.ticks.length; i++) sum += this.ticks[i].temperature;
-        return sum / this.ticks.length;
-    }
-}
-class Hour {
-    constructor(ticks) {
-        this.ticks = ticks;
-    }
-    temperature() {
-        let sum = 0;
-        for(let i = 0; i < this.ticks.length; i++) sum += this.ticks[i].temperature;
-        return sum / this.ticks.length;
-    }
-}
+            $(this).closest(".menu").data("active", $(this).text());
+            $(this).closest(".menu").trigger("change");
+        });
 
+    // #charts
+        let day = new Array(24);
+        day.fill({
+            "temperature": 30,
+            "humidity": 0.5
+        });
+        let temperature_chart = new Chart($("#temperature").get(0).getContext("2d"), {
+            type: "line",
+            data: {
+                labels: Array.from({length: 24}, (_, i) => `${i + 1}:00`),
+                datasets: [{
+                    label: "",
+                    data: Array.from({length: 24}, (_, i) => day[i]["temperature"]),
+                    backgroundColor: "orange",
+                    borderColor: "red",
+                    borderWidth: 2,
+                    pointStyle: "none",
+                    radius: 2
+                }]
+            },
+            options: {
+                responsive: false,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        suggestedMin: -20,
+                        suggestedMax: 50
+                    }
+                }
+            }
+        }); 
+        let humidity_chart = new Chart($("#humidity").get(0).getContext("2d"), {
+            type: "line",
+            data: {
+                labels: Array.from({length: 24}, (_, i) => `${i + 1}:00`),
+                datasets: [{
+                    label: "",
+                    data: Array.from({length: 24}, (_, i) => day[i]["temperature"]),
+                    backgroundColor: "rgb(135, 206, 250)",
+                    borderColor: "blue", 
+                    borderWidth: 2,
+                    pointStyle: "none",
+                    radius: 2
+                }]
+            },
+            options: {
+                responsive: false,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        suggestedMax: 100
+                    }
+                }
+            }
+        });
+        function update_graphs() {
+            temperature_chart.data.datasets[0].label = 
+                `${($("#language_menu").data("active") == "ENG")? "Temperature": "Temperatura"} (°${$("#degree_menu").data("active")})`
+            ;
+            switch ($("#degree_menu").data("active")) {
+                case "C":
+                    temperature_chart.data.datasets[0].data = Array.from({length: 24}, (_, i) => day[i]["temperature"]);
+                    temperature_chart.options.scales.y.suggestedMin = -20;
+                    temperature_chart.options.scales.y.suggestedMax = 50;
+                    break;
+                case "F":
+                    temperature_chart.data.datasets[0].data = Array.from({length: 24}, (_, i) => (day[i]["temperature"] * 9/5) + 32);
+                    temperature_chart.options.scales.y.suggestedMin = 0;
+                    temperature_chart.options.scales.y.suggestedMax = 120;
+                    break;
+                case "K":
+                    temperature_chart.data.datasets[0].data = Array.from({length: 24}, (_, i) => day[i]["temperature"] +  273.15);
+                    temperature_chart.options.scales.y.suggestedMin = 250;
+                    temperature_chart.options.scales.y.suggestedMax = 320;
+                    break;
+                default:
+                    break;
+            }
+            temperature_chart.update();
+            humidity_chart.data.datasets[0].label = `${($("#language_menu").data("active") == "ENG")? "Humidity": "Umidità"} (%RH)`;
+            humidity_chart.update();
+        }
+        update_graphs();
 
-(60 * 60) / 10
+    // #day_selection
+        function update_day_list() {
+            $("#day_list *").each(function() {
+                $(this).html(/* html */ `
+                    <div class="text_div"
+                        data-ENG="05 / 10 / 2023 <br> Sunday"
+                        data-ITA="05 / 10 / 2023 <br> Domenica"
+                    >
+                    <img src="images/sunny.png">
+                    <div class="text_div">30°${$("#degree_menu").data("active")} 50%RH</div>
+                `);
+            });
+        }
+        $("#degree_menu").on("change", function() {
+            update_day_list();
+            update_graphs();
+        });
+        update_day_list();
 
-// data uploaded every 10 seconds
-class Tick {
-    constructor(weather, temperature) {
-        this.weather = weather;
-        this.temperature = temperature;
-    }
-}
-
-const today = new Date();
-const shown_day_cards = [
-    new Day(today, "SUNNY", 30),
-    new Day(new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1), "SUNNY", 30),
-    new Day(new Date(today.getFullYear(), today.getMonth(), today.getDate() + 2), "SUNNY", 30),
-    new Day(new Date(today.getFullYear(), today.getMonth(), today.getDate() + 3), "SUNNY", 30),
-    new Day(new Date(today.getFullYear(), today.getMonth(), today.getDate() + 4), "SUNNY", 30),
-    new Day(new Date(today.getFullYear(), today.getMonth(), today.getDate() + 5), "SUNNY", 30),
-    new Day(new Date(today.getFullYear(), today.getMonth(), today.getDate() + 6), "SUNNY", 30)
-];
-
-document.addEventListener("DOMContentLoaded", function () {
-    $("#day_cards div").each(function(index) {
-        $(this).html(shown_day_cards[index].card());
-    });
+    // #language_menu
+        function update_language() {
+            $(`div[data-${$("#language_menu").data("active")}]`).each(function() {
+                $(this).html($(this).data($("#language_menu").data("active")));
+            });
+            update_graphs();
+        }
+        $("#language_menu").on("change", update_language);
+        update_language();
 });
