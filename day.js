c65f325c81f1409a007b1e2779a36a8e41a60119
PinePunk16 <106622968+PinePunk16@users.noreply.github.com>
2023-12-01 10:36:59 +0100

    added day.js and week.js

---
+++ day.js
@@ -0,0 +1,59 @@
+function day(date) {
+    $("#nav_week").removeClass("selected");
+    $("#nav_day").addClass("selected");
+    $("main").html( /* html */ `
+        <div>${date.toString()}</div>
+    `);
+    let temperature_chart = new Chart($("#temperature_chart").getContext("2d"), {
+        type: "line",
+        data: {
+            labels: current_week.dayStrings,
+            datasets: [{
+                label: "Temperature",
+                data: current_week.temperatures,
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 2,
+                fill: false
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    type: "category",
+                    position: "bottom"
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    let humidity_chart = new Chart($("#humidity_chart").getContext("2d"), {
+        type: "line",
+        data: {
+            labels: current_week.dayStrings,
+            datasets: [{
+                label: "Temperature",
+                data: current_week.temperatures,
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 2,
+                fill: false
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    type: "category",
+                    position: "bottom"
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
