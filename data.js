7f4be8decccd031e66b41249dfdf2e84a6c07903
GIANLUCA GALANTI <s_glnglc06e28l500q@itis-emattei.local>
2023-12-01 10:32:25 +0100

    tempAndHum

--- data.js
+++ data.js
@@ -3,6 +3,12 @@
     (per qualche motivo this è obbligatorio in javascript)
 */
 
+var dataUrl = new URL("http://10.25.0.14:3000/misurazioni?data_ora");
+var currentResponse;
+var temperature_data = [], humidity_data = [];
+var tempIndex = 0, humIndex = 0;
+var currentDate = new Date(), dataDay;
+
 const Weather = {
     SUNNY: "sunny",
     VEILED: "veiled",
@@ -11,6 +17,7 @@ const Weather = {
     SNOW: "snow",
     NO_DATA: "no_data"
 };
+
 class Day {
     constructor(date) {
         this.date = new Date(date.toISOString().split("T")[0]);
@@ -18,22 +25,53 @@ class Day {
         this._humidity = null;
         this._weather = null;
     }
-    get temperature() {
-        return 30;
+
+    temperature(response) 
+    {
+        tempIndex = 0;
+
+        do
+        {
+            currentResponse = response.pop();
+            dataDay = currentResponse["data_ora"].slice(8, 10);
+            if(currentResponse["tipo"] === "TEMPERATURA") {
+                temperature_data[tempIndex] = currentResponse["valore"];
+                tempIndex++;
+            }
+        }while(dataDay != currentDate.getDate());
+        return 0;
     }
-    get humidity() {
-        return 50;
+    
+    humidity(response) 
+    {
+        humIndex = 0;
+
+        do
+        {
+            currentResponse = response.pop();
+            dataDay = currentResponse["data_ora"].slice(8, 10);
+            if (currentResponse["tipo"] === "UMIDITA") {
+                humidity_data[humIndex] = currentResponse["valore"];
+                humIndex++;
+            }
+        }while(dataDay != currentDate.getDate());
+        return 0;
     }
+    
     get weather() {
         return Weather.CLOUDY;
     }
+    
     monthString() {
         return new Intl.DateTimeFormat("en-US", { month: "long" }).format(this.date);
     }
+    
     weekdayString() {
         return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(this.date);
     }
-    suffix() {
+    
+    suffix() 
+    {
         if(Math.floor(this.date.getDate()) != this.date.getDate()) return "";
         if(this.date.getDate() >= 11 && this.date.getDate() <= 13) return "th";
         switch(this.date.getDate() % 10) {
@@ -47,7 +85,31 @@ class Day {
                 return "th";
         }
     }
+
+    currentConditions()
+    {
+        dataUrl = new URL("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=Urbino,PU,61034&aggregateHours=24&lang=it&unitGroup=metric&shortColumnNames=false&contentType=json&key=EVPUPWJLED7AAULMJBMDVB3GJ");
+        fetchData();
+    }
+    
+    fetchData(ev)
+    {
+        fetch(dataUrl)
+            .then(response => {
+                if (!response.ok)
+                    throw new Error("Something went wrong");
+                return response.json();
+            })
+            .then((response) => {
+                temperature(response);
+                humidity(response);
+            })
+            .catch(console.warn);
+    }
+
 }
+
+
 class Time {
     constructor(date) {
         this.date = new Date(date.toString());
