84f913ff5253d6deed1e07ca3a31e3979c6d4d0d
Gianlu2805 <86599945+Gianlu2805@users.noreply.github.com>
2024-01-08 11:43:13 +0100

    Update apiData.js

--- scripts/apiData.js
+++ scripts/apiData.js
@@ -1,4 +1,4 @@
-const apiDataUrl = new URL("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=Urbino,PU,61034&aggregateHours=24&lang=it&unitGroup=metric&shortColumnNames=false&contentType=json&key=EVPUPWJLED7AAULMJBMDVB3GJ");
+const apiDataUrl = new URL("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=Urbino,PU,61029&aggregateHours=24&lang=it&unitGroup=metric&shortColumnNames=false&contentType=json&key=EVPUPWJLED7AAULMJBMDVB3GJ");
 
 
 async function updateForecastData() {
@@ -10,10 +10,10 @@ async function updateForecastData() {
         })
         .then(function(response) {
             for(var i = 0; i < 16; i++) {
-                forecastData.dates[i] = new Date(response.locations["Urbino,PU,61034"].values[i].datetimeStr);
-                forecastData.weather[i] = response.locations["Urbino,PU,61034"].values[i].conditions;
-                forecastData.temperatures[i] = response.locations["Urbino,PU,61034"].values[i].temp;
-                forecastData.humidities[i] = response.locations["Urbino,PU,61034"].values[i].humidity;
+                forecastData.dates[i] = new Date(response.locations["Urbino,PU,61029"].values[i].datetimeStr);
+                forecastData.weather[i] = response.locations["Urbino,PU,61029"].values[i].conditions;
+                forecastData.temperatures[i] = response.locations["Urbino,PU,61029"].values[i].temp;
+                forecastData.humidities[i] = response.locations["Urbino,PU,61029"].values[i].humidity;
             }
         })
         .catch(function(error) {
