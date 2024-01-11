4af6af8a3f5dd2b37f4f6e8c1cd6bee77ac04d80
ghrlcu06e20l500g <s_ghrlcu06e20l500g@itisurbino.it>
2024-01-11 20:45:06 +0100

    put async in update functions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const hours = [
     "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00",
 ];
 
-function showForecast() {
+async function showForecast() {
     $("#loading_screen").show();
 
     await updateForecastData();
@@ -48,7 +48,7 @@ function showForecast() {
 }
 
 var selectedDate = null;
-function showHistory() {
+async function showHistory() {
     $("#loading_screen").show();
 
     await updateHistoryData(selectedDate);
