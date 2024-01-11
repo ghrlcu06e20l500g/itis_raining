d709a432c84dd6239bcb35c2cd20f4303fa8787b
ghrlcu06e20l500g <s_ghrlcu06e20l500g@itisurbino.it>
2024-01-11 20:38:55 +0100

    updated data.js

--- data.js
+++ data.js
@@ -43,5 +43,55 @@ var historyData = [
     { time: new Date(), temperature: 30, humidity: 50 }
 ];
 
-function updateForecastData() {}
-function updateHistoryData(selectedDate) {}
+const forecastUrl = new URL("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=Urbino,PU,61029&aggregateHours=24&lang=it&unitGroup=metric&shortColumnNames=false&contentType=json&key=EVPUPWJLED7AAULMJBMDVB3GJ");
+const historyUrl = new URL("http://10.25.0.14:3000/misurazioni?data_ora");
+
+function updateForecastData() {
+    return new Promise(function(resolve, reject) {
+        fetch(forecastUrl)
+        .then(function(response) {
+            if(!response.ok) throw new Error();
+            else return response.json();
+        })
+        .then(function(response) {
+            for(var i = 0; i < 16; i++) {
+                forecastData[i].date = new Date(response.locations["Urbino,PU,61029"].values[i].datetimeStr);
+                forecastData[i].weather = response.locations["Urbino,PU,61029"].values[i].conditions;
+                forecastData[i].temperature = response.locations["Urbino,PU,61029"].values[i].temp;
+                forecastData[i].humidity = response.locations["Urbino,PU,61029"].values[i].humidity;
+            }
+        })
+        .catch(function(error) {
+            console.error(error);
+            $("#error_data").html(error.toString());
+            $("#error").show();
+        });
+    });
+}
+function updateHistoryData(selectedDate) {
+    return new Promise(function(resolve, reject) {
+        fetch(serverDataUrl)
+            .then(function(response) {
+                if(!response.ok) throw new Error();
+                return response.json();
+            })
+            .then(function(response) {
+                var i = 0;
+                do {
+                    var currentResponse = response.pop();
+                    var dataDay = currentResponse["data_ora"].slice(8, 10);
+                    
+                    historyData[i].date = currentResponse["data_ora"];
+                    if(currentResponse.tipo == "UMIDITA") historyData[i].humidity = currentResponse['valore'];
+                    else if(currentResponse.tipo == "TEMPERATURA") historyData[i].temperature = currentResponse['valore'];
+                    i++;
+                } while(dataDay == selectedDate.toString().slice(8, 10));
+            })
+            .catch(function(error) {
+                console.error(error);
+                $("#error_data").html(error.toString());
+                $("#error").show();
+            });
+    });
+}
+
