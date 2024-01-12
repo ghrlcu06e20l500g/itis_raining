69f570b438b3d19297ea18679e201da1798db712
Gianlu2805 <86599945+Gianlu2805@users.noreply.github.com>
2024-01-12 17:51:51 +0100

    added personal keyphrase

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*itismattei = chiave del certificato per openssl certificato LASICARE QUI PER ORA*/
+
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const hours = [
     "00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00",
