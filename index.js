9f130b23c52b1c7178baa004fe6c8881fcc4666d
ghrlcu06e20l500g <s_ghrlcu06e20l500g@itisurbino.it>
2024-01-12 11:01:29 +0100

    fixed bg name and forecast day order

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const hours = [
     "00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00",
     "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00",
@@ -11,10 +11,6 @@ async function showForecast() {
 
     $("#nav_history").removeClass("selected");
     $("#nav_forecast").addClass("selected");
-
-    // non so perché ieri va ma oggi no per settare .current però ok.
-    var yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
 
     var days = "";
     for(day of forecastData) {
@@ -33,7 +29,7 @@ async function showForecast() {
         days += /* html */ `
             <div 
                 style="background-image: url('images/backgrounds/${day.weather.split(",")[0].split(" ")[0]}.png');"
-                class="${(day.date.toISOString().split('T')[0] == yesterday.toISOString().split('T')[0])? 'current' : ''}"
+                class="${(day.date.toISOString().split('T')[0] == new Date.toISOString().split('T')[0])? 'current' : ''}"
             >
                 <div weekday>${weekdays[day.date.getDay()]}</div>
                 <div date>${day.date.toISOString().split('T')[0]}</div>
