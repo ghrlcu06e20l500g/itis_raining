4916c50a4920be3d7e513c7655310f14582e2741
ghrlcu06e20l500g <s_ghrlcu06e20l500g@itisurbino.it>
2024-01-12 10:05:10 +0100

    fixed query

--- data.js
+++ data.js
@@ -67,10 +67,7 @@ async function updateForecastData() {
 }
 async function updateHistoryData(selectedDate) {
     for(var i = 0; i < 24; i++) {
-        fetch(new URL(`http://10.25.0.14:3000/misurazioni?
-            data_ora=gte.${selectedDate.toISOString().split('T')[0]}T${String(i).padStart(2, '0')}:00:00&
-            data_ora=lt.${selectedDate.toISOString().split('T')[0]}T${(i + 1 < 24)? String(i + 1).padStart(2, '0'): "00"}:00:00`
-        )).then(function(response) {
+        fetch(new URL(`http://10.25.0.14:3000/misurazioni?data_ora=gte.${selectedDate.toISOString().split('T')[0]}T${String(i).padStart(2, '0')}:00:00&data_ora=lt.${selectedDate.toISOString().split('T')[0]}T${(i + 1 < 24)? String(i + 1).padStart(2, '0'): "00"}:00:00`)).then(function(response) {
                 if(!response.ok) throw new Error();
                 return response.json();
             })
